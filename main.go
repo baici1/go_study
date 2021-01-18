ebc327acfc8dff938c1c7cc551a2215e32661be7
baici1 <249337001@qq.com>
2021-01-19 00:09:44 +0800

    beng

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ type User struct {
 	var err error
 	dsn:="root:123456@tcp(127.0.0.1:3306)/sql_test"
 	//使用mysql驱动
-	DB,err := sql.Open("mysql", dsn)
+	DB,err = sql.Open("mysql", dsn)
 	if err != nil {
 		return err
 	}
@@ -35,19 +35,86 @@ type User struct {
 
 //CURD
 // 查询单条数据示例
-func queryResult() {
-	querySQL := "SELECT `id`,`name`,`age` FROM `test_user` WHERE `id`=?"
-	// 执行查询语句
-	row := DB.QueryRow(querySQL, 1)
-    // 定义 user 为结构体 User 类型
-	var user User
-	// 此处获取结果的顺序一定要和 SELECT 语句取出的顺序一样
-	err := row.Scan(&user.Id, &user.Name, &user.Age)
+func queryRowDemo()  {
+	sqlStr:="select id,name,age from test_user where id=?"
+	var u User
+	err := DB.QueryRow(sqlStr,1).Scan(&u.Id,&u.Name,&u.Age)
 	if err != nil {
-		fmt.Printf("query result failed, err: %v\n", err)
+		fmt.Printf("scan failed, err:%v\n", err)
+		return	
+	}
+	fmt.Printf("id:%d name:%s age:%d\n",u.Id,u.Name,u.Age)
+}
+
+
+//查询多条数据
+func queryMultiRowDemo(id int)  {
+	sqlStr:="select id, name, age from test_user where id >= ?"
+	rows,err:= DB.Query(sqlStr,id)
+	if err != nil {
+		fmt.Printf("query failed, err:%v\n", err)
+		return	
+	}
+	// 非常重要：关闭rows释放持有的数据库链接
+	defer rows.Close()
+	for rows.Next(){
+		var u User
+		err:=rows.Scan(&u.Id,&u.Name,&u.Age)
+		if err != nil {
+			fmt.Printf("scan failed, err:%v\n", err)
+			return
+		}
+		fmt.Printf("id:%d name:%s age:%d\n",u.Id,u.Name,u.Age)
+	}
+}
+
+
+//插入、更新和删除操作都使用Exec方法。
+//插入数据
+func insertRowDemo(name string,age int)  {
+	sqlStr:="insert into test_user(name, age) values (?,?)"
+	ret,err:=DB.Exec(sqlStr,name,age)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
 		return
 	}
-	fmt.Printf("query result: id=%d name=%s age=%d\n", user.Id, user.Name, user.Age)
+	theid ,err:=ret.LastInsertId()
+	if err != nil {
+		fmt.Printf("get lastinsert ID failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success, the id is %d.\n", theid)
+}
+
+//更新数据
+func updateRowDemo(age int,id int)  {
+	sqlStr:="update test_user set age=? where id = ?"
+	ret,err:= DB.Exec(sqlStr,age,id)
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return
+	}
+	n,err:= ret.RowsAffected()
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("update success, affected rows:%d\n", n)
+}
+//删除数据
+func deleteRowDemo(id int)  {
+	sqlStr:="delete from test_user where id = ?"
+	ret,err:=DB.Exec(sqlStr,id)
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return
+	}
+	n,err:= ret.RowsAffected()
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success, affected rows:%d\n", n)
 }
 
 
@@ -59,5 +126,9 @@ func main() {
 	}else {
 		fmt.Println("success")
 	}
-	 queryResult()
+	queryRowDemo()
+	queryMultiRowDemo(1)
+	insertRowDemo("yayyy",21)
+	updateRowDemo(18,3)
+	deleteRowDemo(1)
 }
