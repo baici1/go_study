23378739fc53f7b87cc9be677fdf72b3ad0f135a
baici1 <249337001@qq.com>
2021-01-24 22:35:08 +0800

    2021/1/24

--- router/router.go
+++ router/router.go
@@ -1,19 +1,20 @@
 package router
 
 import (
-	handler_jwt "example.com/m/v2/HandlerFunc"
 	"example.com/m/v2/api"
+	"example.com/m/v2/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 
 func InitRoute() {
 	router:=gin.Default()
-
+	router.Use(middlewares.Cors())
 	v:=router.Group("admin")
 	{
-		v.GET("/login",handler_jwt.JWTAuthMiddleware, api.Login)
+		v.POST("/login", api.Login)
 		v.POST("/register",api.Register)
 	}
-	router.Run()
+	router.Run(":8090")
+	
 }
